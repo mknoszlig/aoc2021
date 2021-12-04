9a6caed89df5c2281687d54f9b2413c0b6151182
Maximilian Karasz <mknoszlig@zeroton.in>
2021-12-04 14:41:27 +0100

    day 4.

---
+++ src/aoc2021/day4.clj
@@ -0,0 +1,65 @@
+(ns day4
+  (:require [clojure.string :as s]
+            [clojure.edn :as edn]
+            [clojure.set :as cs]))
+(defn wrap-edn-vec [str]
+  (edn/read-string (format "[%s]" str)))
+
+(defn prepare-board [b]
+  (->> (concat b (apply map vector b))
+       (mapv set)))
+
+(defn parse-input [[h _ & boards]]
+  {:numbers (wrap-edn-vec h)
+   :drawn   []
+   :boards  (->> (filter (comp not empty?) boards)
+                 (mapv wrap-edn-vec)
+                 (partition 5)
+                 (mapv prepare-board))})
+
+(def input (-> (s/split-lines (slurp "resources/day4.txt"))
+               parse-input))
+
+
+(defn bingo? [rowcols]
+  (some empty? rowcols))
+
+(defn score [{:keys [boards drawn]}]
+  (* (last drawn)
+     (->> (filter bingo? boards)
+          first
+          (apply concat)
+          (into #{})
+          (apply +))))
+
+(defn update-board [rowcols draw]
+  (mapv #(disj % draw) rowcols))
+
+(defn step [{:keys [numbers drawn boards] :as state}]
+  (let [[draw & more] numbers]
+    (if draw
+      {:numbers more
+       :drawn  (conj drawn draw)
+       :boards (mapv #(update-board % draw) boards)}
+      state)))
+
+(defn rm-step [state]
+  (-> (step state)
+      (update :boards #(remove bingo? %))))
+
+(defn find-bingo [init-state]
+  (->> (iterate step input)
+       (drop-while #(not-any? bingo? (:boards %)))
+       first))
+
+(defn find-last-bingo [init-state]
+  (->> (iterate rm-step input)
+       (drop-while #(> (count (:boards %)) 1))
+       first
+       (iterate step)
+       (drop-while #(not-any? bingo? (:boards %)))
+       first))
+
+(printf "part 1: %s\n" (score (find-bingo input)))
+
+(printf "part 2: %s\n" (score (find-last-bingo input)))
