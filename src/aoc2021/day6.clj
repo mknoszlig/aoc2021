d63e9d822ab1f0fee4c165ee11e97170ccbc20d1
Maximilian Karasz <mknoszlig@zeroton.in>
2021-12-06 10:17:38 +0100

    no need to reimplement frequencies :)

--- src/aoc2021/day6.clj
+++ src/aoc2021/day6.clj
@@ -4,9 +4,6 @@
 
 (def input (edn/read-string (format "[%s]" (slurp "resources/day6.txt"))))
 
-(defn init [input]
-  (reduce #(update %1 %2 (fnil inc 0)) {} input))
-
 (defn update-state [state]
   (let [spawns (get state 0 0)]
     (-> (reduce #(assoc %1 (dec %2) (get %1 %2 0)) state (range 1 9))
@@ -14,7 +11,7 @@
         (assoc 8 spawns))))
 
 (defn fish-after-gens [n]
-  (->> (iterate update-state (init input))
+  (->> (iterate update-state (frequencies input))
        (take (inc n))
        last
        vals
