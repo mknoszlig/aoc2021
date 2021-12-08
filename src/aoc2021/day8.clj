011ca8925989cbd452b3af9e6a2217e236d7f4e4
Maximilian Karasz <mknoszlig@zeroton.in>
2021-12-08 20:24:35 +0100

    day8 improved.

--- src/aoc2021/day8.clj
+++ src/aoc2021/day8.clj
@@ -7,106 +7,70 @@
        (mapv (fn [s] (reduce #(conj %1 (keyword (str %2))) #{} s)))))
 
 (defn str->input [[patterns display]]
-  {:digits (strs->sets patterns)
+  {:digits  (strs->sets patterns)
    :display (strs->sets display)})
 
 (def input (->> (s/split-lines (slurp "resources/day8.txt"))
                 (mapv #(s/split % #" \| "))
-                (mapv str->input)
-                ))
+                (mapv str->input)))
 
+(def segments->digit
+  {#{:a :b :c :e :f :g}    0
+   #{:c :f}                1
+   #{:a :c :d :e :g}       2
+   #{:a :c :d :f :g}       3
+   #{:b :c :d :f}          4
+   #{:a :b :d :f :g}       5
+   #{:a :b :d :e :f :g}    6
+   #{:a :c :f}             7
+   #{:a :b :c :d :e :f :g} 8
+   #{:a :b :c :d :f :g}    9})
 
-(def digit->segments
-  {0 #{:a :b :c :e :f :g}
-   1 #{:c :f}
-   2 #{:a :c :d :e :g}
-   3 #{:a :c :d :f :g}
-   4 #{:b :c :d :f}
-   5 #{:a :b :d :f :g}
-   6 #{:a :b :d :e :f :g}
-   7 #{:a :c :f}
-   8 #{:a :b :c :d :e :f :g}
-   9 #{:a :b :c :d :f :g}})
+(defn is-matches? [mapping p p']
+  (->> mapping
+       (drop-while (fn [[s s']] (= (count (cs/intersection p s))
+                                   (count (cs/intersection p' s')))))
+       (empty?)))
 
-(defn seg-count [n]
-  (fn [digits] (filter #(= n (count %)) digits)))
+(defn join-sorted-by-size [d d']
+  (let [groupd  (group-by count d)
+        groupd' (group-by count d')
+        max     (apply max (keys groupd))]
+    (->> (map (juxt groupd groupd') (range (inc max)))
+         (filter (partial not= [nil nil]))
+         (take max))))
 
-(defn is-size [n other]
-  (fn [digits] (filter #(= n (count (cs/intersection % other)))
-                       digits)))
+(defn map-intersections [seed ds ds']
+  (reduce (fn [acc [d d']]
+            (if (is-matches? acc d d')
+              (assoc acc d d')
+              acc))
+          seed
+          (mapcat #(mapv (partial vector %) ds') ds)))
 
-(defn but-not [other]
-  (fn [digits] (filter #(not= % other) digits)))
+(defn map-sub-groups [seed [d d']]
+  (cond
+    (= 1 (count d) (count d'))
+    (assoc seed (first d) (first d'))
+    :else
+    (map-intersections seed d d')))
 
-(defn d1 [digits]
-  (first (-> digits ((seg-count 2)))))
-
-(defn d4 [digits]
-  (first (-> digits ((seg-count 4)))))
-
-(defn d7 [digits]
-  (first (filter #(= 3 (count %)) digits)))
-
-(defn d8 [digits]
-  (first (filter #(= 7 (count %)) digits)))
-
-(defn d0 [digits]
-  (-> digits
-      ((seg-count 6))
-      ((is-size 2 (d1 digits)))
-      ((is-size 3 (d4 digits)))
-      first))
-
-(defn d2 [digits]
-  (-> digits
-      ((seg-count 5))
-      ((is-size 2 (d4 digits)))
-      first))
-
-(defn d3 [digits]
-  (-> digits
-      ((seg-count 5))
-      ((is-size 4 (d2 digits)))
-      first))
-
-(defn d5 [digits]
-  (-> digits
-      ((seg-count 5))
-      ((is-size 3 (d2 digits)))
-      first))
-
-(defn d9 [digits]
-  (-> digits
-      ((seg-count 6))
-      ((is-size 4 (d4 digits)))
-      first))
-
-(defn d6 [digits]
-  (-> digits
-      ((seg-count 6))
-      ((but-not (d9 digits)))
-      ((but-not (d0 digits)))
-      first))
+(defn map-to-origin [m]
+  (reduce (fn [acc [d d']]
+                   (assoc acc d' (segments->digit d)))
+          {}
+          m))
 
 (defn deduce-digits [digits]
-  {(d0 digits) 0
-   (d1 digits) 1
-   (d2 digits) 2
-   (d3 digits) 3
-   (d4 digits) 4
-   (d5 digits) 5
-   (d6 digits) 6
-   (d7 digits) 7
-   (d8 digits) 8
-   (d9 digits) 9})
+  (->> (reduce map-sub-groups {} (join-sorted-by-size (keys segments->digit) digits))
+       (map-to-origin)))
 
 (defn part-1 [input]
   (->> (mapcat (fn [{:keys [digits display]}]
                  (mapv (deduce-digits digits) display))
                input)
        (filter #{1 4 7 8})
-       count
-       ))
+       count))
 
 (defn part-2 [input]
   (->> (mapv (fn [{:keys [digits display]}]
@@ -118,3 +82,23 @@
 
 (printf "part 1: %s\n" (time (part-1 input)))
 (printf "part 2: %s\n" (time (part-2 input)))
+
+;; > time clojure -M src/aoc2021/day8.clj
+;; "Elapsed time: 55.161213 msecs"
+;; part 1: 261
+;; "Elapsed time: 52.043766 msecs"
+;; part 2: 987553
+
+;; real	0m1.128s
+;; user	0m2.525s
+;; sys	0m0.182s
+
+;; > time bb src/aoc2021/day8.clj
+;; "Elapsed time: 63.358904 msecs"
+;; part 1: 261
+;; "Elapsed time: 72.496108 msecs"
+;; part 2: 987553
+
+;; real	0m0.179s
+;; user	0m0.135s
+;; sys	0m0.039s
